0df50c42e934d089a86bb638d7e72494350660f2
Thanh C Pham <burukuru@gmail.com>
2019-01-15 21:57:02 +0000

    Use multiprocessing and wget to download images.
    Rate limit API requests to 1 per second.

--- fad.py
+++ fad.py
@@ -5,8 +5,11 @@ import json
 import os
 import urllib
 import urllib.parse
+import shutil
 import csv
 import configparser
+from multiprocessing import Pool
+from ratelimit import limits, sleep_and_retry
 
 # Config
 class FAD():
@@ -31,56 +34,83 @@ class FAD():
 
     def make_dir(self, dest_dir):
         try:
-            os.makedirs(self.dest_dir, 0o755, exist_ok=True)
+            os.makedirs(dest_dir, 0o755, exist_ok=True)
         except OSError:
-            print (f"Creation of directory {self.dest_dir} failed.")
+            print (f"Creation of directory {dest_dir} failed.")
+            pass
         else:
-            print (f"Creation of directory {self.dest_dir} succeeded.")
+            print (f"Creation of directory {dest_dir} succeeded.")
+            pass
 
-    def download_pics(self):
+    def download_all_albums(self):
         # Create parent dir
         self.make_dir(self.dest_dir)
 
-        for set in self.sets['photosets']['photoset']:
+        album_and_photolist = [ [set['title']['_content'], list(self.get_photo_list(set['id']))] for set in self.sets['photosets']['photoset'] ]
 
-            print(set['title']['_content'])
+        pool = Pool(5)
+        results = pool.map(self.download_album, album_and_photolist)
+        pool.close()
+        pool.join()
 
-            # Create album directory
-            album_dir = os.path.join(self.dest_dir, set['title']['_content'])
-            self.make_dir(album_dir)
 
-            photos = self.flickr_etree.walk_set(set['id'], extras='url_o')
+    def download_album(self, album_and_photolist):
+        print(album_and_photolist[0])
 
-            # TODO
-            # - write album API info to file
-            # album_api_response = open(os.path.join(self.dest_dir, 'album_api_response.txt'), 'w')
-            # album_api_response.write(photos)
+        # Create album directory
+        album_dir = os.path.join(self.dest_dir, album_and_photolist[0])
+        self.make_dir(album_dir)
 
-            # Create index to order photos
-            index = 1
+        # TODO
+        # - write album API info to file
+        # album_api_response = open(os.path.join(self.dest_dir, 'album_api_response.txt'), 'w')
+        # album_api_response.write(photos)
+
+        # Create index to order photos
+        index = 1
+        try:
             os.remove(os.path.join(album_dir, 'album.csv'))
-            csvfile = open(os.path.join(album_dir, 'album.csv'), 'a')
-            csvwriter = csv.writer(csvfile)
+        except:
+            pass
+        csvfile = open(os.path.join(album_dir, 'album.csv'), 'a')
+        csvwriter = csv.writer(csvfile)
 
-            for photo in photos:
-                url = photo.get('url_o')
-                print(url)
-                url_ = urllib.parse.urlparse(url)
-                filename = str(index) + '_' + os.path.basename(url_.path)
-                title = photo.get('title')
+        for photo in album_and_photolist[1]:
+            url = photo.get('url_o')
+            print(url)
+            url_ = urllib.parse.urlparse(url)
+            filename = str(index).zfill(3) + '_' + os.path.basename(url_.path)
+            title = photo.get('title')
 
-                # Write photo title to CSV file
-                csvwriter.writerow([str(index), filename, title])
-                full_dest_path = os.path.join(album_dir, filename)
-                # Download file
-                urllib.request.urlretrieve(url, full_dest_path)
-                index += 1
-            csvfile.close()
-            break
+            # Write photo title to CSV file
+            csvwriter.writerow([str(index).zfill(3), filename, title])
+            full_dest_path = os.path.join(album_dir, filename)
+            # Download file
+            self.download_photo(url, album_dir, full_dest_path)
+            index += 1
+        csvfile.close()
+
+    def download_photo(self, url, album_dir, full_dest_path):
+        try:
+            # urllib.request.urlretrieve(url, full_dest_path)
+
+            # response = urllib.request.urlopen(url)
+            # out_file = open(full_dest_path, 'wb')
+            # shutil.copyfileobj(response, out_file)
+            os.system(f'wget --continue {url} -O "{full_dest_path}" -a "{album_dir}/download.log" --random-wait --waitretry=5 --retry-connrefused --retry-on-host-error')
+        except urllib.error.URLError as e:
+            print(f"{url} failed to download")
+            print(e)
+
+    @sleep_and_retry
+    @limits(calls=1, period=1)
+    def get_photo_list(self, album_id):
+        photos = self.flickr_etree.walk_set(album_id, extras='url_o')
+        return photos
 
 def main():
     fad = FAD()
-    fad.download_pics()
+    fad.download_all_albums()
 
 if __name__ == '__main__':
     main()
