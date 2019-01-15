3757e24c40873f8fe6e9595de344304f459fa0b0
Thanh C Pham <burukuru@gmail.com>
2019-01-15 22:04:55 +0000

    --wip--

--- fad.py
+++ fad.py
@@ -7,6 +7,7 @@ import urllib
 import urllib.parse
 import csv
 import configparser
+from multiprocessing import Pool as ThreadPool
 
 # Config
 class FAD():
@@ -31,56 +32,65 @@ class FAD():
 
     def make_dir(self, dest_dir):
         try:
-            os.makedirs(self.dest_dir, 0o755, exist_ok=True)
+            os.makedirs(dest_dir, 0o755, exist_ok=True)
         except OSError:
-            print (f"Creation of directory {self.dest_dir} failed.")
+            print (f"Creation of directory {dest_dir} failed.")
         else:
-            print (f"Creation of directory {self.dest_dir} succeeded.")
+            print (f"Creation of directory {dest_dir} succeeded.")
 
-    def download_pics(self):
+    def download_all_albums(self):
         # Create parent dir
         self.make_dir(self.dest_dir)
 
-        for set in self.sets['photosets']['photoset']:
+        pool = ThreadPool(2)
+        results = pool.map(self.download_album, [self.sets['photosets']['photoset'][0], self.sets['photosets']['photoset'][1]])
 
-            print(set['title']['_content'])
+    def download_album(self, set):
+        print(set['title']['_content'])
 
-            # Create album directory
-            album_dir = os.path.join(self.dest_dir, set['title']['_content'])
-            self.make_dir(album_dir)
+        # Create album directory
+        album_title = set['title']['_content']
+        album_dir = os.path.join(self.dest_dir, album_title)
+        self.make_dir(album_dir)
 
-            photos = self.flickr_etree.walk_set(set['id'], extras='url_o')
+        photos = self.flickr_etree.walk_set(set['id'], extras='url_o')
 
-            # TODO
-            # - write album API info to file
-            # album_api_response = open(os.path.join(self.dest_dir, 'album_api_response.txt'), 'w')
-            # album_api_response.write(photos)
+        # TODO
+        # - write album API info to file
+        # album_api_response = open(os.path.join(self.dest_dir, 'album_api_response.txt'), 'w')
+        # album_api_response.write(photos)
 
-            # Create index to order photos
-            index = 1
+        # Create index to order photos
+        index = 1
+        try:
             os.remove(os.path.join(album_dir, 'album.csv'))
-            csvfile = open(os.path.join(album_dir, 'album.csv'), 'a')
-            csvwriter = csv.writer(csvfile)
+        except:
+            print('album.csv does not exist.')
 
-            for photo in photos:
-                url = photo.get('url_o')
-                print(url)
-                url_ = urllib.parse.urlparse(url)
-                filename = str(index) + '_' + os.path.basename(url_.path)
-                title = photo.get('title')
+        csvfile = open(os.path.join(album_dir, 'album.csv'), 'a')
+        csvwriter = csv.writer(csvfile)
 
-                # Write photo title to CSV file
-                csvwriter.writerow([str(index), filename, title])
-                full_dest_path = os.path.join(album_dir, filename)
-                # Download file
+        for photo in photos:
+            url = photo.get('url_o')
+            print(f"{album_title}: {url}")
+            url_ = urllib.parse.urlparse(url)
+            filename = str(index) + '_' + os.path.basename(url_.path)
+            title = photo.get('title')
+
+            # Write photo title to CSV file
+            csvwriter.writerow([str(index), filename, title])
+            full_dest_path = os.path.join(album_dir, filename)
+            # Download file
+            try:
                 urllib.request.urlretrieve(url, full_dest_path)
                 index += 1
-            csvfile.close()
-            break
+            except urllib.error.ContentTooShortError as e:
+                print(f"{album_title}: {e.msg} - {e.content}")
+        csvfile.close()
 
 def main():
     fad = FAD()
-    fad.download_pics()
+    fad.download_all_albums()
 
 if __name__ == '__main__':
     main()
